220458d1a2ab580b6cd4edeca08c986d8ca5f9d8
John P Considine <jconsidi@nd.edu>
2017-12-10 12:34:56 -0600

    feat() token based verification test() tested new verification refactor() extracted out optoins for authentication

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var request = require("request");
 var cookie = require('cookie');
 
+
+
+var baseHeader  = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Connection': 'keep-alive'
+};
 module.exports = {
   /**
    *
@@ -11,55 +17,62 @@ module.exports = {
    * @param  {string} endpoint absolute WP REST API endpoint
    * @return {Array}  all cookies
    */
-  authenticateAndGetCookie : function(endpoint, username, psswd) {
-
-    var options = {
-        method: 'POST',
-        uri: endpoint,
-        simple: false,
-        form: {
-            log : username,
-            pwd : psswd
-        },
-        headers: {
-            /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
-        }
-    };
+  authenticateWithCredentials : function(endpoint, options) {
     return new Promise(function(resolve, reject) {
-      request(options, function(error, response, body) {
-        if (! error) {
-          try {
-            var cookies = response.headers['set-cookie'];
-          } catch(e) {
-            error = e;
+      if (! options || (! options.jsonbody && ! options.formbody )) throw new Error ("options must contain auth credentials");
+      var reqoptions = {
+          method: 'POST',
+          uri: endpoint,
+          simple: false,
+          form: options.formbody,
+          body : options.jsonbody,
+          json : !!options.jsonbody,
+          headers: {
+              /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
           }
-        }
+      };
+      request(reqoptions, function(error, response, body) {
         if (error) return reject(error);
-        resolve(cookies);
+        resolve(response);
       })
     })
   },
-
+  extractCookie : function (resp) {
+    if (! resp || ! resp.headers || ! resp.headers["set-cookie"] ) throw new Error("Response has no cookie!");
+    return resp.headers['set-cookie'];
+  },
   restAPIRequestWithAuthenticationCookie : function (options) {
     var options = (options) ? options : {};
     var endpoint = options.endpoint;
     var authcookie = (options.authcookie) ? options.authcookie : "";
 
-    var headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Cookie': authcookie,
-        'Connection': 'keep-alive'
-    };
-
+    var headers = JSON.parse(JSON.stringify(baseHeader));
+    headers["Cookie"] = authcookie;
     return new Promise(function(resolve, reject) {
       if (! endpoint ) reject(new Error("no endpoint supplied!"));
       request({headers  : headers, uri : endpoint}, function(error, response, body) {
         if (error) return reject(error);
+        if ((response.statusCode + "")[0] == '4') reject("Authentication failed")
         resolve(body);
       });
     })
-
+  },
+  restAPIRequestWithToken : function (options) {
+    var headers = JSON.parse(JSON.stringify(baseHeader));
+    options = (options) ? options : {};
+    var endpoint = options.endpoint;
+    var token = options.token;
+    if (! token || ! endpoint) {
+      throw new Error("options requires an 'endpoint' and 'token'");
+    }
+    headers["Authorization"] = "Bearer " + token;
+    // console.log(headers);
+    return new Promise(function(resolve, reject) {
+      request({headers: headers, uri : endpoint}, function(error, response, body) {
+        if (error) reject(error);
+        if ((response.statusCode + "")[0] == '4') reject("Authentication failed")
+        resolve(body);
+      });
+    });
   }
-
-
 }
